8deb0e8d7992d6ff3792d93e7a1cced1e65b3241
BlackoutRoulette <blackoutroulette@live.de>
2025-07-28 12:08:25 +0200

    Added packet_adapter tests

--- tests/parsing/__init__.py
+++ tests/parsing/__init__.py
@@ -6,12 +6,33 @@ from archipelago_py import packets
 PATH = Path(__file__).parent
 
 T = TypeVar("T", bound=packets.Packet)
-def parse_json(file: Path, packet_cls: type[T]) -> T:
-
-    data: str = file.read_text(encoding="utf-8")
-    assert data # Ensure the file is not empty
+def helper_test_parse_json(data: str, packet_cls: type[T]) -> T:
 
     packet = packet_cls.model_validate_json(json_data=data)
     assert isinstance(packet, packet_cls)
 
     return packet
+
+def helper_test_packet_adapter(data: str, packet_cls: type[T]) -> T:
+    packet = packets.PACKET_TYPE_ADAPTER.validate_json(f"[{data}]")
+
+    assert isinstance(packet, list)
+    assert len(packet) == 1
+    assert isinstance(packet[0], packet_cls)
+
+    return packet[0]
+
+def load_data(folder: Path, recursive: bool = False) -> list[tuple[Path, str]]:
+    files = folder.rglob("*.json") if recursive else folder.glob("*.json")
+    assert files, f"No JSON files found in {PATH/folder}"
+
+    data = [
+        (p, p.read_text("utf-8"))
+        for p
+        in files
+    ]
+
+    for (p, d) in data:
+        assert d, f"File {p} is empty"
+
+    return data
---
+++ tests/parsing/test_print_json/release/test_release.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+from typing import Final
+
+import pytest
+
+from archipelago_py import packets, enums
+from archipelago_py.enums import PrintJSONType
+from tests.parsing import load_data, helper_test_packet_adapter, helper_test_parse_json
+
+TEST_DATA: Final[list[tuple[Path, str]]] = load_data(Path(__file__).parent)
+PACKET_TYPE: Final = packets.PrintJSON
+PRINT_JSON_TYPE: Final[enums.PrintJSONType] = PrintJSONType.RELEASE
+
+@pytest.mark.parametrize("data", TEST_DATA, ids=lambda p: p[0].name)
+def test_model(data: tuple[Path, str]):
+    packet = helper_test_parse_json(data[1], PACKET_TYPE)
+    assert packet.type == PRINT_JSON_TYPE
+
+@pytest.mark.parametrize("data", TEST_DATA, ids=lambda p: p[0].name)
+def test_packet_adapter(data: tuple[Path, str]):
+    helper_test_packet_adapter(data[1], PACKET_TYPE)
